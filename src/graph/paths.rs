4a1a9e3d02e48489a261e8c38cf0c8cbfa84f7eb
Felix Wiegand <fxwiegand@wgdnet.de>
2025-08-25 16:58:19 +0200

    fix: Fix contains_variant to take target string argument (#137)

--- src/graph/paths.rs
+++ src/graph/paths.rs
@@ -43,17 +43,19 @@ impl Cds {
     }
 
     /// Returns true if the CDS contains the variant
-    pub(crate) fn contains_variant(&self, variants: &HashMap<String, BTreeSet<i64>>) -> bool {
-        for variant in variants.values() {
-            if variant
+    pub(crate) fn contains_variant(
+        &self,
+        target: &str,
+        variants: &HashMap<String, BTreeSet<i64>>,
+    ) -> bool {
+        if let Some(variant_positions) = variants.get(target) {
+            variant_positions
                 .range(self.start as i64..=self.end as i64)
                 .next()
                 .is_some()
-            {
-                return true;
-            }
+        } else {
+            false
         }
-        false
     }
 
     /// Length of CDS segment
@@ -320,39 +322,44 @@ mod tests {
     #[test]
     fn contains_variant_returns_true_when_variant_in_range() {
         let cds = Cds::new(10, 20, 0);
+        let target = "chr1";
         let mut variants = HashMap::new();
-        variants.insert("variant1".to_string(), BTreeSet::from([15]));
-        assert!(cds.contains_variant(&variants));
+        variants.insert(target.to_string(), BTreeSet::from([15]));
+        assert!(cds.contains_variant(target, &variants));
     }
 
     #[test]
     fn contains_variant_returns_false_when_variant_out_of_range() {
         let cds = Cds::new(10, 20, 0);
+        let target = "chr1";
         let mut variants = HashMap::new();
-        variants.insert("variant1".to_string(), BTreeSet::from([25]));
-        assert!(!cds.contains_variant(&variants));
+        variants.insert(target.to_string(), BTreeSet::from([25]));
+        assert!(!cds.contains_variant(target, &variants));
     }
 
     #[test]
     fn contains_variant_returns_false_when_no_variants() {
         let cds = Cds::new(10, 20, 0);
+        let target = "chr1";
         let variants: HashMap<String, BTreeSet<i64>> = HashMap::new();
-        assert!(!cds.contains_variant(&variants));
+        assert!(!cds.contains_variant(target, &variants));
     }
 
     #[test]
     fn contains_variant_returns_true_when_multiple_variants_in_range() {
         let cds = Cds::new(10, 20, 0);
+        let target = "chr1";
         let mut variants = HashMap::new();
-        variants.insert("variant1".to_string(), BTreeSet::from([12, 18]));
-        assert!(cds.contains_variant(&variants));
+        variants.insert(target.to_string(), BTreeSet::from([12, 18]));
+        assert!(cds.contains_variant(target, &variants));
     }
 
     #[test]
     fn contains_variant_returns_true_when_variants_on_boundary() {
         let cds = Cds::new(10, 20, 0);
+        let target = "chr1";
         let mut variants = HashMap::new();
-        variants.insert("variant1".to_string(), BTreeSet::from([10, 20]));
-        assert!(cds.contains_variant(&variants));
+        variants.insert(target.to_string(), BTreeSet::from([10, 20]));
+        assert!(cds.contains_variant(target, &variants));
     }
 }
