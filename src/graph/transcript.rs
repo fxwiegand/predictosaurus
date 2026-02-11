9726647e37faaa4beb2b02def3b912de217935bb
Felix Wiegand <fxwiegand@wgdnet.de>
2026-02-11 22:54:19 +0100

    fix: Fix combinatory extension of CDSs (#195)
    * fix: Fix combinatory extension of CDSs
    
    * Skip merging when CDS RNA is empty

--- src/graph/transcript.rs
+++ src/graph/transcript.rs
@@ -189,6 +189,8 @@ impl Transcript {
 
                 if haplotypes.is_empty() {
                     haplotypes = paths;
+                } else if paths.is_empty() {
+                    continue;
                 } else {
                     let mut extended = Vec::new();
 
@@ -435,6 +437,8 @@ impl Transcript {
             }
             if rna.is_empty() {
                 rna = cds_rna;
+            } else if cds_rna.is_empty() {
+                continue;
             } else {
                 let mut new_rna = Vec::new();
                 for rna_path in &rna {
