423f1f3a5e2d6deb6b519558898f35103800cb47
fxwiegand <fxwiegand@wgdnet.de>
2026-04-27 10:35:14 +0200

    fix: Skip failed annotations

--- src/annotation.rs
+++ src/annotation.rs
@@ -1,7 +1,10 @@
 use anyhow::Result;
 use bio::bio_types::strand::Strand;
-use genebears::{AnnotateOptions, ClientConfig, GeneBears, Genome, Variant};
+use genebears::{
+    AnnotateOptions, AnnotatedVariant, ClientConfig, GeneBearError, GeneBears, Genome, Variant,
+};
 use itertools::Itertools;
+use log::warn;
 use serde::{Deserialize, Serialize};
 
 use crate::graph::node::Node;
@@ -50,11 +53,28 @@ impl Annotation {
             })
             .collect_vec();
         let client = GeneBears::new(ClientConfig::default())?;
-        let results = tokio::runtime::Runtime::new()?.block_on(client.annotate_variants(
-            &variants,
-            genome_build,
-            AnnotateOptions::default(),
-        ))?;
+        let rt = tokio::runtime::Runtime::new()?;
+
+        let results: Vec<AnnotatedVariant> = variants
+            .iter()
+            .filter_map(|variant| {
+                match rt.block_on(client.annotate_variant(
+                    variant,
+                    genome_build,
+                    AnnotateOptions::default(),
+                )) {
+                    Ok(ann) => Some(Ok(ann)),
+                    Err(GeneBearError::ApiServerError { message, .. }) => {
+                        warn!(
+                            "GeneBe Error trying to annotate variant {}{}>{}: {}",
+                            variant.pos, variant.ref_allele, variant.alt_allele, message
+                        );
+                        None
+                    }
+                    Err(e) => Some(Err(anyhow::Error::from(e))),
+                }
+            })
+            .collect::<Result<Vec<_>>>()?;
 
         Ok(Self {
             revel_score: max_score(results.iter().map(|r| r.revel_score)),
